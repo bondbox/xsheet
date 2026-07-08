3caeccbe4a6c4df01372a5b73fa5760c0093d92b
Mingzhe Zou <zoumingzhe@qq.com>
2026-07-08 20:50:34 +0800

    bump version from 0.2 to 0.3

--- xkits_sheet/attribute.py
+++ xkits_sheet/attribute.py
@@ -6,6 +6,6 @@ __project_home__ = "https://github.com/bondbox/xsheet/"
 __project_desc__ = "Form module"
 
 # package info
-__version__ = "0.2"
+__version__ = "0.3"
 __package_name__ = "xkits-sheet"
 __package_desc__ = "Form module"
